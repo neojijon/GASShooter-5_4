36485f0c65e8a92075eb2e0cdd6234ab92868628
neojijon <1780556+neojijon@users.noreply.github.com>
2024-08-22 21:12:40 +0900

    Add files via upload
    UE5.3 -> UE5_4 마이그레이션

---
+++ Source/Private/UI/GSHUDReticle.cpp
@@ -0,0 +1,5 @@
+// Copyright 2024 Dan Kestranek.
+
+
+#include "UI/GSHUDReticle.h"
+
---
+++ Source/Public/Weapons/GSProjectile.h
@@ -0,0 +1,21 @@
+// Copyright 2024 Dan Kestranek.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GSProjectile.generated.h"
+
+UCLASS()
+class GASSHOOTER_API AGSProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGSProjectile();
+
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "PBProjectile")
+	class UProjectileMovementComponent* ProjectileMovement;
+};
